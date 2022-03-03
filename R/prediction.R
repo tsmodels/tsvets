d52ff9326edb0142445ccccd92d821d6917a54f9
Alexios Galanos <alexios@4dscape.com>
2022-03-03 13:34:54 -0800

    fix to init_states argument checks in predict

--- R/prediction.R
+++ R/prediction.R
@@ -35,7 +35,7 @@ predict.tsvets.estimate <- function(object, h = 12, newxreg = NULL, nsim = 1000,
     date_class <- attr(object$spec$target$sampling, "date_class")
     xseed <- tail(object$States, 1)
     if (!is.null(init_states)) {
-        if (length(as.vector(init_states) != ncol(xseed))) {
+        if (length(as.vector(init_states)) != ncol(xseed)) {
             stop(paste0("\ninit_states must be a vector of length ", ncol(xseed)))
         } else {
             xseed <- matrix(as.numeric(init_states), nrow = 1, ncol = ncol(xseed))
